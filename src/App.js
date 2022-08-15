6997d97fa600a11b3f7cd161525a686c0b45122f
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2022-08-15 22:16:34 +0200

    styled-icons/bootstrap

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import { Routes, Route } from "react-router-dom";
 
-import { Home, Icons, Interactive, Characters, AIArt } from './pages'
-import { Navbar } from './components'
+import { Home, Icons, Interactive, Characters, AIArt } from "./pages";
+import { Navbar } from "./components";
 
-
-
-import './App.css';
-
-
+import "./App.css";
 
 function App() {
   return (
-
-
-
     <Wrapper>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path='/icons' element={<Icons />} />
-        <Route path='/characters' element={<Characters />} />
-        <Route path='/interactive' element={<Interactive />} />
-        <Route path='/aiart' element={<AIArt />} />
-
+        <Route path="/icons" element={<Icons />} />
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/interactive" element={<Interactive />} />
+        <Route path="/aiart" element={<AIArt />} />
       </Routes>
-
     </Wrapper>
-
-
-
-
   );
 }
-
-
-
 
 const Wrapper = styled.div`
   font-family: "Spline Sans", sans-serif;
@@ -46,7 +30,6 @@ const Wrapper = styled.div`
   position: relative;
   height: 100%;
   overflow-x: hidden;
- 
 `;
 
-export default App
+export default App;
